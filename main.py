5efe4d1e00a31847a9db5b2d650a52bf6f86bbdc
josecarlosmemo <misterio205.1@gmail.com>
2022-03-25 11:24:23 -0600

    Version Final

--- main.py
+++ main.py
@@ -1,27 +1,80 @@
 import cv2
 from time import time
 import mediapipe as mp
+import itertools
+import numpy as np
 
-face_detection = mp.solutions.face_detection.FaceDetection(model_selection=0,min_detection_confidence=0.5)
-mp_drawing = mp.solutions.drawing_utils
-mp_styles = mp.solutions.drawing_styles
-face_mesh  = mp.solutions.face_mesh.FaceMesh(static_image_mode=False,max_num_faces=1,min_detection_confidence=0.5,min_tracking_confidence=0.3)
+face_mesh = mp.solutions.face_mesh.FaceMesh(static_image_mode=False, max_num_faces=2, min_detection_confidence=0.5,
+                                            min_tracking_confidence=0.3)
+sonrisa_luismi = cv2.imread('/Users/josecarlos/PycharmProjects/semana_tec_filtro/assets/sonrisa_luismi.png')
+ojo_der = cv2.imread('/Users/josecarlos/PycharmProjects/semana_tec_filtro/assets/ojo_der.png')
+ojo_izq = cv2.imread('/Users/josecarlos/PycharmProjects/semana_tec_filtro/assets/ojo_izq.png')
 
-def facialLandmarks(frame):
+
+def getSize(face_landmarks, INDEXES):
+    INDEXES_LIST = list(itertools.chain(*INDEXES))
+    landmarks = []
+
+    for i in INDEXES_LIST:
+        landmarks.append([int(face_landmarks.landmark[i].x * width),
+                          int(face_landmarks.landmark[i].y * height)])
+
+    landmarks = np.array(landmarks)
+
+    x, y, width_landmark, height_landmark = cv2.boundingRect(landmarks)
+
+    return width_landmark, height_landmark, landmarks
+
+
+def overlay(frame, filter, landmarks, INDEXES):
+    try:
+        filter_h, filter_w, _ = filter.shape
+        _, part_height, landmarks = getSize(landmarks, INDEXES)
+        required_height = int(part_height * 1.5)
+        resized_filter_img = cv2.resize(filter, (int(filter_w * (required_height / filter_h)), required_height))
+        filter_h, filter_w, _ = resized_filter_img.shape
+
+        _, filter_img_mask = cv2.threshold(cv2.cvtColor(resized_filter_img, cv2.COLOR_BGR2GRAY),
+                                           25, 255, cv2.THRESH_BINARY_INV)
+        center = landmarks.mean(axis=0).astype("int")
+
+        location = (int(center[0] - filter_w / 2), int(center[1] - filter_h / 2))
+
+        ROI = frame[location[1]: location[1] + filter_h, location[0]: location[0] + filter_w]
+
+        resultant_image = cv2.bitwise_and(ROI, ROI, mask=filter_img_mask)
+
+        resultant_image = cv2.add(resultant_image, resized_filter_img)
+
+        frame[location[1]: location[1] + filter_h,
+        location[0]: location[0] + filter_w] = resultant_image
+    except Exception as e:
+        pass
+
+
+
+
+
+
+def sad_luismi_filter(frame):
     results = face_mesh.process(frame)
+
     if results.multi_face_landmarks:
-        for landmarks in results.multi_face_landmarks:
-            mp_drawing.draw_landmarks(image=frame,landmark_list=landmarks,connections=mp.solutions.face_mesh.FACEMESH_TESSELATION,landmark_drawing_spec=None,connection_drawing_spec=mp_styles.get_default_face_mesh_tesselation_style())
-            mp_drawing.draw_landmarks(image=frame,landmark_list=landmarks,connections=mp.solutions.face_mesh.FACEMESH_CONTOURS,landmark_drawing_spec=None,connection_drawing_spec=mp_styles.get_default_face_mesh_contours_style())
 
-    return frame
-
+        for face, face_landmarks in enumerate(results.multi_face_landmarks):
+            overlay(frame=frame, landmarks=face_landmarks, filter=sonrisa_luismi,
+                    INDEXES=mp.solutions.face_mesh.FACEMESH_LIPS)
+            overlay(frame=frame, landmarks=face_landmarks, filter=ojo_der,
+                    INDEXES=mp.solutions.face_mesh.FACEMESH_RIGHT_EYE)
+            overlay(frame=frame, landmarks=face_landmarks, filter=ojo_izq,
+                    INDEXES=mp.solutions.face_mesh.FACEMESH_LEFT_EYE)
 
 
 
 
 
 previousTime = 0
+sad_luismi =  True
 if __name__ == '__main__':
 
     # Selección de Camara
@@ -29,15 +82,25 @@ if __name__ == '__main__':
 
     while True:
         # Capturamos Feed
-        ret, frame = cap.read()
+        success, frame = cap.read()
 
+
+        # Si no logramos conseguir la siguiente frame
+        if not success:
+            continue
         # Alto y Ancho de la frame
         width = int(cap.get(3))
         height = int(cap.get(4))
 
-        # TODO Analisis de la Imagen
 
-        frame = facialLandmarks(frame)
+        # Activación de filtros
+        if cv2.waitKey(1) == ord('w'):
+            sad_luismi = not sad_luismi
+
+
+        if sad_luismi:
+            sad_luismi_filter(frame)
+
 
         '''Constantes a Dibujar al Final'''
         # Contador de FPS
